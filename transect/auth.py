9e57a684f7853fd630eec8b70c8676cadd567010
cwih <chris.williams@imagine-have.xyz>
2018-08-11 10:19:33 +0000

    register user

--- transect/auth.py
+++ transect/auth.py
@@ -62,9 +62,7 @@ def load_logged_in_user():
     if user_id is None:
         g.user = None
     else:
-        g.user = get_db().execute(
-            'SELECT * FROM user WHERE id = ?', (user_id,)
-        ).fetchone()
+        g.user = db['users'].find_one({"user_id":user_id})
         
         
 @bp.route('/logout')
