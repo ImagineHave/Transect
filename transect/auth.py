1ae903d30907e3ee7d5ec97fa0514e6c5eaa9542
cwih <chris.williams@imagine-have.xyz>
2018-08-11 12:44:41 +0000

    home

--- transect/auth.py
+++ transect/auth.py
@@ -58,9 +58,6 @@ def login():
 @bp.before_app_request
 def load_logged_in_user():
     user_id = session.get('user_id')
-
-    print (user_id)
-
     if user_id is None:
         g.username = None
     else:
@@ -79,7 +76,5 @@ def login_required(view):
     def wrapped_view(**kwargs):
         if g.username is None:
             return redirect(url_for('auth.login'))
-
         return view(**kwargs)
-
     return wrapped_view
