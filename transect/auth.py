2d23acb518068778a83357f4fa17d14d11527e0d
cwih <chris.williams@imagine-have.xyz>
2018-08-11 12:48:59 +0000

    home

--- transect/auth.py
+++ transect/auth.py
@@ -67,7 +67,7 @@ def load_logged_in_user():
 @bp.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('auth/login.html'))
+    return redirect(url_for('auth/login'))
     
     
 def login_required(view):
