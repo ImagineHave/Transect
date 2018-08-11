8f6b691690d2d913748255e282f66e100dae2808
cwih <chris.williams@imagine-have.xyz>
2018-08-11 23:26:20 +0000

    wtf

--- transect/auth.py
+++ transect/auth.py
@@ -8,7 +8,7 @@ from transect.db import (
     get_user, set_user, get_username, get_userid, check_password_for_user
 )
 
-from app.forms import LoginForm
+from transect.forms.auth.login import LoginForm
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
