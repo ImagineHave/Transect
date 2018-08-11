9497da7c3f4ee00399b85cc048589754133cc29c
cwih <chris.williams@imagine-have.xyz>
2018-08-11 10:12:57 +0000

    register user

--- transect/auth.py
+++ transect/auth.py
@@ -17,20 +17,16 @@ def register():
         db = get_db()
         error = None
 
+
+
         if not username:
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-        elif db.execute(
-            'SELECT id FROM user WHERE username = ?', (username,)
-        ).fetchone() is not None:
+        elif db['users'].find_one({"username":username}) is not None:
             error = 'User {} is already registered.'.format(username)
-
         if error is None:
-            db.execute(
-                'INSERT INTO user (username, password) VALUES (?, ?)',
-                (username, generate_password_hash(password))
-            )
+            db.insert_one({"username":username,"password":generate_password_hash(password)})
             db.commit()
             return redirect(url_for('auth.login'))
 
