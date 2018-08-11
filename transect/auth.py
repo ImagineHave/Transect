c9982a0c8f321f5e6698bd9ad74a8ab16efccdf5
cwih <chris.williams@imagine-have.xyz>
2018-08-11 12:40:31 +0000

    home

--- transect/auth.py
+++ transect/auth.py
@@ -54,11 +54,12 @@ def login():
             
     return render_template('auth/login.html')
 
+
 @bp.before_app_request
 def load_logged_in_user():
     user_id = session.get('user_id')
 
-    print user_id
+    print (user_id)
 
     if user_id is None:
         g.username = None
