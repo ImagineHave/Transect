eea97c5cc0456334a505d6741eab65225c241f66
cwih <chris.williams@imagine-have.xyz>
2018-08-11 10:26:06 +0000

    register user

--- transect/auth.py
+++ transect/auth.py
@@ -47,7 +47,7 @@ def login():
 
         if error is None:
             session.clear()
-            session['user_id'] = user['id']
+            session['user_id'] = user['_id']
             return redirect(url_for('index'))
 
         flash(error)
