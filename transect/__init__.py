4116fcb097e0b44b180c567ac306f517146e1d1a
cwih <chris.williams@imagine-have.xyz>
2018-08-12 14:34:11 +0000

    fixing register

--- transect/__init__.py
+++ transect/__init__.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask
+from flask_wtf.csrf import CSRFProtect
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    csrf = CSRFProtect(app)
     
     app.config.from_mapping(
             SECRET_KEY = os.environ['SECRET_KEY'],
