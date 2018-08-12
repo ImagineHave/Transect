62d154e683b4d3f62e9bd66d88567332bc5c8a5f
cwih <chris.williams@imagine-have.xyz>
2018-08-12 15:01:11 +0000

    fixing register

--- transect/__init__.py
+++ transect/__init__.py
@@ -44,4 +44,4 @@ def create_app(test_config=None):
     return app
     
 def handle_csrf_error(e):
-    return render_template('csrf_error.html', reason=e.description), 400
+    return render_template('csrf_error.html', reason=e.description)
