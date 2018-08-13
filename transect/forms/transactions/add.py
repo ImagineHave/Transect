e2ab1c9d05f115ca0ee5a3e48f38580431f03377
cwih <chris.williams@imagine-have.xyz>
2018-08-13 18:37:11 +0000

    fixing transactions

--- transect/forms/transactions/add.py
+++ transect/forms/transactions/add.py
@@ -2,10 +2,11 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, DecimalField, DateField
 from wtforms.validators import DataRequired
 import datetime
+from decimal import ROUND_HALF_UP
 
 class AddForm(FlaskForm):
     payer = StringField('payer', [DataRequired()])
     payee = StringField('payee', [DataRequired()])
-    amount = DecimalField('amount', [DataRequired()])
+    amount = DecimalField('amount', [DataRequired()], places=2, default=0.0, rounding=ROUND_HALF_UP)
     date = DateField('date', [DataRequired()], format='%Y-%m-%d', default=datetime.date.today())
     submit = SubmitField('add')
